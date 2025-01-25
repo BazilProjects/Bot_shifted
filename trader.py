d4e09a9bc005d0b9c11a87cd6eb5fc246d14e6ef
ForexOL <zantamore@gmail.com>
2025-01-25 12:34:54 +0300

    Update trader.py

--- trader.py
+++ trader.py
@@ -296,5 +296,5 @@ async def main2():
     except Exception as e:
         raise e
         print(f"An error occurred: {e}")
-#def main():
-asyncio.run(main2())
+def main():
+    asyncio.run(main2())
